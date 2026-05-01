4a12703b2eda8e7a4a1881255563852efea2f0cf
brownie-jpeg <114025648+brownie-jpeg@users.noreply.github.com>
2026-05-01 14:03:00 -0500

    refactored WeaponData.cs to include public access variables

--- Assets/Scripts/WeaponData.cs
+++ Assets/Scripts/WeaponData.cs
@@ -16,29 +16,46 @@ public enum WeaponFireMode
 public class WeaponData : ScriptableObject
 {
     [Header("Info")]
-    public string weaponName;
-
-    public Sprite icon;
-    public int cost;
+    [SerializeField] private string weaponName;
+    [SerializeField] private Sprite icon;
+    [SerializeField] private int cost;
 
     [Header("Combat")] 
-    public WeaponDamageType damageType = WeaponDamageType.Ranged;
-
-    public WeaponFireMode fireMode = WeaponFireMode.SemiAuto;
-    public int damage = 10;
-    public float range = 5f;
-    public float fireSpread = 0f;
-    public float attacksPerSecond = 2f;
-    public LayerMask attackLayerMask;
+    [SerializeField] private WeaponDamageType damageType = WeaponDamageType.Range;
+    [SerializeField] private WeaponFireMode fireMode = WeaponFireMode.SemiAuto;
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float range = 5f;
+    [SerializeField] private float fireSpread = 0f;
+    [SerializeField] private float attacksPerSecond = 2f;
+    [SerializeField] private LayerMask attackLayerMask;
     
     [Header("Ammo")]
-    public bool usesAmmo = true;
-
-    public int magSize = 16;
-    public float reloadTime = 1.5f;
+    [SerializeField] private bool usesAmmo = true;
+    [SerializeField] private int magSize = 16;
+    [SerializeField] private float reloadTime = 1.5f;
 
     [Header("Audio")]
-    public AudioClip attackSfx;
-    public AudioClip reloadStartSfx;
-    public AudioClip reloadFinishSfx;
+    [SerializeField] private AudioClip attackSfx;
+    [SerializeField] private AudioClip reloadStartSfx;
+    [SerializeField] private AudioClip reloadFinishSfx;
+
+    public string WeaponName => weaponName;
+    public Sprite Icon => icon;
+    public int Cost => cost;
+
+    public WeaponDamageType DamageType => damageType;
+    public WeaponFireMode FireMode => fireMode;
+    public int Damage => damage;
+    public float Range => range;
+    public float FireSpread => fireSpread;
+    public float AttacksPerSecond => attacksPerSecond;
+    public LayerMask AttackLayerMask => attackLayerMask;
+
+    public bool UsesAmmo => usesAmmo;
+    public int MagSize => magSize;
+    public float ReloadTime => reloadTime;
+
+    public AudioClip AttackSfx => attackSfx;
+    public AudioClip ReloadStartSfx => reloadStartSfx;
+    public AudioClip ReloadFinishSfx => reloadFinishSfx;
 }
